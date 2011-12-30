c624c4833460a63d06326f563ad6d899aecec9bc
Zeljko <zeljko@z-ware.fi>
2011-12-30 14:18:38 +0200

    Added license text everywhere.

--- application/helpers/zen_passwords.php
+++ application/helpers/zen_passwords.php
@@ -1,17 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-  function zen_validate_password($plain, $encrypted) {
-    if (zen_not_null($plain) && zen_not_null($encrypted)) {
-// split apart the hash / salt
-      $stack = explode(':', $encrypted);
-
-      if (sizeof($stack) != 2) return false;
-
-      if (md5($stack[1] . $plain) == $stack[0]) {
-        return true;
-      }
-    }
-
-    return false;
+  function validate_password_zen_way($password, $zen_md5) {
+      $split = explode(':', $zen_md5);
+      return (sizeof($split) == 2 && (md5($split[1] . $password) == $split[0]));
   }
--- application/libraries/OrderHandler.php
+++ application/libraries/OrderHandler.php
@@ -1,5 +1,42 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
-
+/**
+ * ZOM Connector
+ *
+ * An open source application for webshops based on PHP.
+ * Based on the CodeIgniter framework. Visit http://codeigniter.com for more info.
+ *
+ * @package		ZOM Connector
+ * @author		Z-Ware Ltd.
+ * @copyright	Copyright (c) 2011, Z-Ware Ltd.
+ * @license		http://zom.z-ware.fi/license
+ * @link		http://zom.z-ware.fi
+ * @since		Version 0.0.1
+ * @filesource
+ *  
+ * Copyright (C) 2011 Z-Ware Ltd.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * This Software is based on the CodeIgniter framework (codeigniter.com).
+ * Any use/change/distribution of the Software must comply with the Code Igniter 
+ * license (http://codeigniter.com/user_guide/license.html).
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 class OrderHandler {
   function __construct($db_order = null) {
      if(!$db_order) {
